f8c079760e3a9bbc7a0647206d4df4aee50ab0eb
Enei <eneipereira@outlook.com.br>
2022-08-01 19:13:12 -0300

    fix: Altera nome das variáveis de ambiente para corrigir erro no CI

--- src/vars.ts
+++ src/vars.ts
@@ -1,9 +1,9 @@
 const { env } = process;
 
 export default {
-  username: env.DB_USER,
-  password: env.DB_PASSWD,
-  host: env.DB_HOST,
+  username: env.MYSQL_USER,
+  password: env.MYSQL_PASSWORD,
+  host: env.MYSQL_HOST,
   apiPort: Number(env.PORT || 3000),
-  jwtSecret: env.JWT_SECRET,
+  jwtSecret: env.JWT_SECRET || 'sanduichedepresunto',
 };
